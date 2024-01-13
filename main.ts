ae276e87a909862f52556c1b85620bc156062cdb
sambetant <104916257+sambetant@users.noreply.github.com>
2024-01-13 21:37:22 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,48 @@
+radio.setGroup(1)
+basic.forever(function () {
+    if (Math.abs(input.acceleration(Dimension.Y)) < 100 && Math.abs(input.acceleration(Dimension.X)) < 100) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (input.acceleration(Dimension.X) < -200) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    if (input.acceleration(Dimension.X) > 200) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+    if (input.acceleration(Dimension.Y) > 200) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (input.acceleration(Dimension.Y) < -200) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+})
